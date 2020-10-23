80ff4b81c38ef68ab188b52c06e60e329643909e
Andre Gomes <andrefilipepereiragomes@gmail.com>
2020-10-23 17:40:34 +0100

    Added firebaseConfig

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //You need to paste here the configurations that firebase gives you when you create the DataBase 
+  apiKey: "AIzaSyBK6_UnRIL_A2i3IjWRPCCns_3yWR6XuHA",
+  authDomain: "whatsapp-clone-16a51.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-16a51.firebaseio.com",
+  projectId: "whatsapp-clone-16a51",
+  storageBucket: "whatsapp-clone-16a51.appspot.com",
+  messagingSenderId: "80532423050",
+  appId: "1:80532423050:web:d2f11ae82c9906e7dc4873",
+  measurementId: "G-8D5GNCQEQD"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
